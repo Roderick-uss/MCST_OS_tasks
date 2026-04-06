8d46d7c1bfd98661d22d1c42e169cfb5b2afe03a
DukeRoderick <Ushakov.Sergeyi@yandex.ru>
2026-04-06 18:11:44 +0300

    frame for tast 5 created

---
+++ headers/parser.h
@@ -0,0 +1,9 @@
+#ifndef __PRSER__
+#define __PRSER__
+
+#include <stdio.h>
+
+const char short_option_list[] = "mcst";
+const char * const long_option_list[] = {"elbrus=1c+" "elbrus=2c+", "elbrus=2c3", "elbrus=4c", "elbrus=8c", "elbrus=16c"};
+
+#endif//__PRSER__
---
+++ sources/main.c
@@ -0,0 +1,6 @@
+#include "common.h"
+#include "parser.h"
+
+int main() {
+    return 0;
+}
